f66ee3736b4a211b09dee794f55647793cb4defa
Russel Arbore <russel.jma@gmail.com>
2022-04-17 11:43:55 -0500

    Fixed lots of compile issues, small graph demo

--- include/graph.h
+++ include/graph.h
@@ -22,45 +22,43 @@ private:
     
     std::unordered_map<V, VertexInfo> vertices;
     std::unordered_map<std::pair<V, V>, EdgeInfo, boost::hash<std::pair<V, V>>> edges;
+
+    struct VertexInfoEdgeMember {
+	EdgeSide es;
+	std::pair<V, V> edge_label;
+	V v;
+    };
     
     struct VertexInfo {
-	unsigned deg;
-	std::list<
-	    std::tuple<
-		EdgeSide,
-		std::pair<V, V>,
-		V
-		>
-	    > vertex_edges;
+        unsigned deg;
+	std::list<VertexInfoEdgeMember> vertex_edges;
     };
 
     struct EdgeInfo {
 	E e;
-	typename decltype(VertexInfo::vertex_edges)::iterator first;
-	typename decltype(VertexInfo::vertex_edges)::iterator second;
+	typename std::list<VertexInfoEdgeMember>::iterator first;
+	typename std::list<VertexInfoEdgeMember>::iterator second;
     };
 };
 
 template <typename V, typename E>
 void Graph<V, E>::insert_vertex(const V& v) {
-    vertices.emplace(v, {0, {}});
+    vertices.insert({v, {0, {}}});
 }
 
 template <typename V, typename E>
 void Graph<V, E>::insert_edge(const E& e, const V& v1, const V& v2) {
-    edges.emplace({v1, v2}, {e, {}, {}});
     ++vertices.at(v1).deg;
-    vertices.at(v1).vertex_edges.emplace_back(EdgeSide::FIRST, edges.at({v1, v2}), v1);
+    vertices.at(v1).vertex_edges.push_front(VertexInfoEdgeMember{EdgeSide::FIRST, {v1, v2}, v1});
     ++vertices.at(v2).deg;
-    vertices.at(v2).vertex_edges.emplace_back(EdgeSide::SECOND, edges.at({v1, v2}), v2);
-    edges.at({v1, v2})->first = vertices.at(v1).vertex_edges.back();
-    edges.at({v1, v2})->second = vertices.at(v2).vertex_edges.back();
+    vertices.at(v2).vertex_edges.push_front(VertexInfoEdgeMember{EdgeSide::SECOND, {v1, v2}, v2});
+    edges.insert({{v1, v2}, EdgeInfo{e, vertices.at(v1).vertex_edges.begin(), vertices.at(v2).vertex_edges.begin()}});
 }
 
 template <typename V, typename E>
 void Graph<V, E>::remove_vertex(const V& v) {
     for (auto ve : vertices.at(v).vertex_edges) {
-	edges.erase(std::get<1>(ve));
+	edges.erase(ve.edge_label);
     }
     vertices.erase(v);
 }
@@ -77,10 +75,9 @@ void Graph<V, E>::remove_edge(const V& v1, const V& v2) {
 
 template <typename V, typename E>
 std::optional<E> Graph<V, E>::are_adjacent(const V& v1, const V& v2) {
-    std::swap(v1, v2);
-    if (vertices.at(v2).deg > vertices.at(v1).deg) std::swap(v1, v2);
-    for (auto ve : vertices.at(v1).vertex_edges) {
-	if (std::get<1>(ve) == std::make_pair(v1, v2)) return edges.at({v1, v2}).e;
+    auto& to_check = vertices.at(v1).deg < vertices.at(v2).deg ? v1 : v2;
+    for (auto ve : vertices.at(to_check).vertex_edges) {
+	if (ve.edge_label == std::make_pair(v1, v2)) return edges.at({v1, v2}).e;
     }
     return {};
 }
--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,19 @@
 
 int main(int argc, char** argv) {
     Graph<std::size_t, double> graph;
-    std::cout << "Hello, world!\n";
+    graph.insert_vertex(0);
+    graph.insert_vertex(1);
+    graph.insert_vertex(2);
+    graph.insert_edge(4.0, 0, 1);
+    graph.insert_edge(8.0, 0, 2);
+
+    auto edge1 = graph.are_adjacent(0, 1);
+    auto edge2 = graph.are_adjacent(0, 2);
+    auto edge3 = graph.are_adjacent(1, 2);
+
+    if (edge1) std::cout << "Edge 1 has value " << edge1.value() << "\n";
+    if (edge2) std::cout << "Edge 2 has value " << edge2.value() << "\n";
+    if (edge3) std::cout << "Edge 3 has value " << edge3.value() << "\n";
+
     return 0;
 }
