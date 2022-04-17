5947123e8881c382649d2e0ca425a355ccfb0336
Russel Arbore <russel.jma@gmail.com>
2022-04-17 10:22:45 -0500

    Insert v/e

--- include/graph.h
+++ include/graph.h
@@ -1,11 +1,15 @@
 #include <vector>
 #include <list>
 
+enum class EdgeSide {
+    FIRST, SECOND
+};
+
 template <typename V, typename E>
 class Graph {
 public:
     void insert_vertex(const V&);
-    void insert_edge(const E&, const V&, const V&);
+    void insert_edge(const E&, std::size_t v1, std::size_t v2);
 private:
     struct EdgeInfo;
     
@@ -13,11 +17,12 @@ private:
 	V v;
 	unsigned deg;
 	std::list<
-	    std::pair<
+	    std::tuple<
+		EdgeSide,
 		typename std::list<EdgeInfo>::iterator,
 		typename std::vector<VertexInfo>::iterator
 		>
-	    > edge;
+	    > edges;
     };
 
     struct EdgeInfo {
@@ -29,3 +34,17 @@ private:
     std::vector<VertexInfo> vertices;
     std::list<EdgeInfo> edges;
 };
+
+template <typename V, typename E>
+void Graph<V, E>::insert_vertex(const V& v) {
+    vertices.emplace_back(v, 0, {});
+}
+
+template <typename V, typename E>
+void Graph<V, E>::insert_edge(const E& e, std::size_t v1, std::size_t v2) {
+    edges.emplace_back(e, vertices.begin() + v1, vertices.begin() + v2);
+    ++vertices.at(v1).deg;
+    vertices.at(v1).edges.emplace_back(EdgeSide::FIRST, edges.back(), vertices.begin() + v1);
+    ++vertices.at(v2).deg;
+    vertices.at(v2).edges.emplace_back(EdgeSide::SECOND, edges.back(), vertices.begin() + v2);
+}
