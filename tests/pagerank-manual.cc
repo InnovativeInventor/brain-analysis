8e245dda20a9568ecd551e763ae81c7594577311
Russel Arbore <russel.jma@gmail.com>
2022-04-23 22:51:10 -0500

    Another manual test (test convergence)

--- tests/pagerank-manual.cc
+++ tests/pagerank-manual.cc
@@ -4,8 +4,7 @@
 #include "graph.h"
 #include "rank.h"
 
-#include<iostream>
-TEST_CASE( "Manually constructed trivial pagerank test 1" "[graph][pagerank]" ) {
+TEST_CASE( "Manually constructed trivial pagerank test" "[graph][pagerank]" ) {
     Graph<std::size_t, double> graph;
 
     graph.insert_vertex(0);
@@ -25,4 +24,28 @@ TEST_CASE( "Manually constructed trivial pagerank test 1" "[graph][pagerank]" ) 
     auto ranks_norm = graph.rank(1, 0.85);
     for (std::size_t i = 0; i < 3; ++i)
 	REQUIRE_SIMILAR( ranks_norm.at(i), ranks.at(i) );
+}
+
+TEST_CASE( "Manually constructed trivial convergence pagerank test" "[graph][pagerank]" ) {
+    Graph<std::size_t, double> graph;
+
+    graph.insert_vertex(0);
+    graph.insert_vertex(1);
+    graph.insert_vertex(2);
+    graph.insert_vertex(3);
+
+    graph.insert_directed_edge(0.3, 1, 0);
+    graph.insert_directed_edge(0.5, 1, 2);
+    graph.insert_directed_edge(0.1, 2, 0);
+    graph.insert_directed_edge(0.7, 0, 3);
+    graph.insert_directed_edge(0.2, 3, 1);
+
+    auto ranks1 = graph.rank(1, 0.85);
+    auto ranks2 = graph.rank(10, 0.85);
+    auto ranks3 = graph.rank(100, 0.85);
+    for (std::size_t i = 0; i < 4; ++i) { // Pagerank, in this scenario, converge upwards - check that d/dx is positive and that d2/dx2 is negative
+	REQUIRE( ranks1.at(i) < ranks2.at(i) );
+	REQUIRE( ranks2.at(i) < ranks3.at(i) );
+	REQUIRE( ranks3.at(i) - ranks2.at(i) < ranks2.at(i) - ranks1.at(i) );
+    }
 }
